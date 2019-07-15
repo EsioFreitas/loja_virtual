c127209afbd7dbcb1547177dd0360bb24ccd9c07
EsioFreitas <esio.gustavo@gmail.com>
2019-07-15 16:24:39 -0300

    Add page view

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:loja_virtual/screens/home_screen.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: "Flutter's Clothing",
       theme: ThemeData(
         primarySwatch: Colors.blue,
+        primaryColor: Color.fromARGB(255, 4, 125, 141)
       ),
-      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
   }
 }
