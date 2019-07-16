8053ec91f3fd47f76a4f3d0cc26c3ca5f6ae8b61
EsioFreitas <esio.gustavo@gmail.com>
2019-07-16 18:58:48 -0300

    Add product data class

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/tabs/home_tab.dart';
+import 'package:loja_virtual/tabs/products_tab.dart';
 import 'package:loja_virtual/widget/custom_drawer.dart';
 
 class HomeScreen extends StatelessWidget {
+  final _pageController = PageController();
+
   @override
   Widget build(BuildContext context) {
-    final _pageController = PageController();
     return PageView(
       controller: _pageController,
       physics: NeverScrollableScrollPhysics(),
@@ -13,7 +15,12 @@ class HomeScreen extends StatelessWidget {
         Scaffold(
           body: HomeTab(),
           drawer: CustomDrawer(_pageController),
-        )
+        ),
+        Scaffold(
+          appBar: AppBar(title: Text('Produtos'), centerTitle: true),
+          drawer: CustomDrawer(_pageController),
+          body: ProductsTab(),
+        ),
       ],
     );
   }
