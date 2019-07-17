7fc1355e8e1a3261e5dc23f69e9d1395e83e1b8d
EsioFreitas <esio.gustavo@gmail.com>
2019-07-17 09:16:52 -0300

    Update Product tab

--- lib/screens/product_screen.dart
+++ lib/screens/product_screen.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/datas/product_data.dart';
 
@@ -12,9 +13,129 @@ class ProductScreen extends StatefulWidget {
 
 class _ProductScreenState extends State<ProductScreen> {
   final ProductData product;
+  String sizeChoose;
+
   _ProductScreenState(this.product);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    Color primary = Theme.of(context).primaryColor;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(product.title),
+        centerTitle: true,
+      ),
+      body: ListView(
+        children: <Widget>[
+          AspectRatio(
+            aspectRatio: 0.95,
+            child: Carousel(
+              images: product.images.map((img) => NetworkImage(img)).toList(),
+              dotSize: 4.0,
+              dotSpacing: 15.0,
+              dotBgColor: Colors.transparent,
+              dotColor: primary,
+              autoplay: false,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Text(
+                  product.title,
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+                  maxLines: 3,
+                ),
+                Text(
+                  "R\$ ${product.price.toStringAsFixed(2)}",
+                  style: TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                      color: primary),
+                  maxLines: 3,
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Text("Tamanho",
+                    style:
+                        TextStyle(fontSize: 16, fontWeight: FontWeight.w500)),
+                SizedBox(
+                  height: 34,
+                  child: GridView(
+                    padding: EdgeInsets.symmetric(vertical: 4),
+                    scrollDirection: Axis.horizontal,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 1,
+                        mainAxisSpacing: 8,
+                        childAspectRatio: 0.5),
+                    children: product.sizes
+                        .map(
+                          (size) => GestureDetector(
+                            onTap: () {
+                              setState(() {
+                                sizeChoose = size;
+                              });
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(4)),
+                                border: Border.all(
+                                    color: size == sizeChoose
+                                        ? primary
+                                        : Colors.grey[500],
+                                    width: 2),
+                              ),
+                              width: 50,
+                              alignment: Alignment.center,
+                              child: Text(
+                                size,
+                                style: TextStyle(
+                                    color: size == sizeChoose
+                                        ? primary
+                                        : Colors.grey[500]),
+                              ),
+                            ),
+                          ),
+                        )
+                        .toList(),
+                  ),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                SizedBox(
+                  height: 44,
+                  child: RaisedButton(
+                    onPressed: sizeChoose != null ? (){} : null,
+                    child: Text(
+                      "Adicionar ao Carrinho",
+                      style: TextStyle(fontSize: 18, color: Colors.white),
+                    ),
+                    color: primary,
+                  ),
+                ),
+                SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  "Descrição:",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500),
+                  maxLines: 3,
+                ),
+                Text(
+                  product.description,
+                  style: TextStyle(fontSize: 16)
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
