015ab2271d2e7431ee348ed0910f686a3dcb239d
EsioFreitas <esio.gustavo@gmail.com>
2019-07-25 22:44:47 -0300

    Create card_price

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:loja_virtual/models/cart_model.dart';
 import 'package:loja_virtual/models/user_model.dart';
 import 'package:loja_virtual/tiles/cart_tile.dart';
+import 'package:loja_virtual/widget/card_price.dart';
 import 'package:loja_virtual/widget/discount_card.dart';
 import 'package:loja_virtual/widget/ship_card.dart';
 import 'package:scoped_model/scoped_model.dart';
@@ -78,6 +79,7 @@ class CartScreen extends StatelessWidget {
               ),
               DiscountCard(),
               ShipCard(),
+              CardPrice((){}),
             ],
           );
         }
